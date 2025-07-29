beba0eafc953cafb512479f09029a2492d96e9c8
Alessandro Repola <a.repola5@studenti.unisannio.it>
2025-07-29 13:02:11 +0200

    Remove html reporter inport from __init__

--- src/pyfault/reporters/__init__.py
+++ src/pyfault/reporters/__init__.py
@@ -3,7 +3,6 @@ Reporters module for generating output reports.
 """
 
 from .csv_reporter import CSVReporter
-from .html_reporter import HTMLReporter
 from .json_reporter import JSONReporter
 
-__all__ = ["CSVReporter", "HTMLReporter", "JSONReporter"]
+__all__ = ["CSVReporter","JSONReporter"]
